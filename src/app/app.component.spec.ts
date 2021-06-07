8c348f1ff6d03b166de68753b76b96cb896f262a
wendychamale <58566059+wendychamale@users.noreply.github.com>
2021-06-07 17:18:34 -0600

    Cambios para prueba fallida

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('practica1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Esta prueba debe ser fallida!');
   });
 });
